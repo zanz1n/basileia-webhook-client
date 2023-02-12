a7788d67be96ca3ed8808249ae11494589486414
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-02-11 22:44:40 -0300

    add first files

---
+++ src/main/java/xyz/basileiamc/plugins/webhookclient/WebhookClient.kt
@@ -0,0 +1,13 @@
+package xyz.basileiamc.plugins.webhookclient
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WebhookClient : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
