772fd42c4171a74739be0020e1eee7dd94c56b81
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-02-18 11:36:27 -0300

    add get job thread

--- src/main/java/xyz/basileiamc/plugins/webhookclient/jobs/GetJob.kt
+++ src/main/java/xyz/basileiamc/plugins/webhookclient/jobs/GetJob.kt
@@ -1,9 +1,28 @@
 package xyz.basileiamc.plugins.webhookclient.jobs
 
+import org.bukkit.BanList
+import org.bukkit.Bukkit
 import org.bukkit.scheduler.BukkitRunnable
+import xyz.basileiamc.plugins.webhookclient.utils.RequestExecutor
+import java.net.http.HttpClient
+import java.time.Duration
 
 class GetJob : BukkitRunnable() {
     override fun run() {
-        return
+        val executor = RequestExecutor(
+            HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .build())
+
+        try {
+            val response = executor.executeAndParseUnbansGet()
+            Bukkit.getLogger().info(response.toString())
+            response.data.forEach { element ->
+                run {
+                    Bukkit.getBanList(BanList.Type.NAME).pardon(element)
+                }
+            }
+        } catch (err: Exception) { PostJob.errorResponse(err) }
     }
 }
