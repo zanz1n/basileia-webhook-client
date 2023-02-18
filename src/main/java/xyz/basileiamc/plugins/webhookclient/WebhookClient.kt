b283a9d214f428871ad39584f9aba1918c122cec
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-02-18 11:37:41 -0300

    add getjob to the project

--- src/main/java/xyz/basileiamc/plugins/webhookclient/WebhookClient.kt
+++ src/main/java/xyz/basileiamc/plugins/webhookclient/WebhookClient.kt
@@ -4,6 +4,7 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import xyz.basileiamc.plugins.webhookclient.jobs.GetJob
 import xyz.basileiamc.plugins.webhookclient.jobs.PostJob
 import xyz.basileiamc.plugins.webhookclient.utils.bukkit.PluginConfig
 import xyz.basileiamc.plugins.webhookclient.utils.bukkit.readBannedUsers
@@ -16,7 +17,7 @@ class WebhookClient : JavaPlugin() {
         logger.info(Json.encodeToString(readBannedUsers()))
 
         PostJob().runTaskTimerAsynchronously(this, 60, PluginConfig.postDelay)
-//        GetJob().runTaskTimerAsynchronously(this, 60, PluginConfig.getDelay)
+        GetJob().runTaskTimerAsynchronously(this, 60, PluginConfig.getDelay)
     }
 
     override fun onDisable() {
